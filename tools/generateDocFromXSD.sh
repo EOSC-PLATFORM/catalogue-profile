8601de74dd81f6d8002933083423048b536f9bf1
Andreas Czerniak <andreas.czerniak@uni-bielefeld.de>
2023-05-24 10:42:22 +0200

    preparation regarding EOSC Profile v4.0

--- tools/generateDocFromXSD.sh
+++ tools/generateDocFromXSD.sh
@@ -4,6 +4,6 @@
 #
 # Automatically generate the entries for the elements category: service, catalogue, and provider
 #
-# 2022 , Bielefeld University Library: Andreas Czerniak, Sabeel Shah
+# 2022-2023 , Bielefeld University Library: Andreas Czerniak, Sabeel Shah
 
-xmlstarlet tr xslt/selectDocumentationsForComplexType.xsl ../schemas/schema1.xsd > ../docs/elementsCatalogue.rst 2>> logs/exceptionTR.log
+xmlstarlet tr xslt/selectDocumentationsForComplexType.xsl ../schemas/schema1.xsd > ../docs/elements.rst 2>> logs/exceptionTR.log
